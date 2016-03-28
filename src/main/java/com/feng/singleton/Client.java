1cb454788dfa95e7693d709b060c84c08824422f
chinafengqiang <chinafengqiang@sina.com>
2016-03-28 13:45:04 +0800

    update 2016-03-28

--- src/main/java/com/feng/singleton/Client.java
+++ src/main/java/com/feng/singleton/Client.java
@@ -2,10 +2,10 @@ package com.feng.singleton;
 
 /**
  * Created by FQ.CHINA on 2016/3/24.
+ * 测试类
  */
 public class Client {
     public static void main(String[] args){
         Singleton5.INSTANCE.print();
-
     }
 }
